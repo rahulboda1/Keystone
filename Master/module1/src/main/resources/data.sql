75217deca7007ed0ccd9574a2a7ca08e864ab7c7
Rahul Boda <rahul.fall15@gmail.com>
2018-06-24 17:01:08 -0500

    added extra details

--- Master/module1/src/main/resources/data.sql
+++ Master/module1/src/main/resources/data.sql
@@ -1,2 +1,3 @@
 insert into RESOURCE values(1,'Doctor','Jeff',102,'Doc-01');
 insert into RESOURCE values(2,'Patient','John',103,'Pat-01');
+insert into RESOURCE values(3,'adsbdPatient','John',103,'Pat-03');
